2337922372f6546489921c40abc650828cdd5b71
jesseumetoma <85384479+jesseumetoma@users.noreply.github.com>
2021-10-14 19:16:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.use(cors())
 
